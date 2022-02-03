d17d04af9230a7a54f7d01d43ff2708775003b92
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2022-02-03 17:28:27 +0600

    :hammer: ExoPlayer is an application level media player for Android. It provides an alternative to Android’s MediaPlayer API for playing audio and video both locally and over the Internet.

---
+++ app/src/main/java/com/coxtunes/coxtuneslivetv/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.coxtunes.coxtuneslivetv
+
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.coxtunes.coxtuneslivetv.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonAudio.setOnClickListener {
+            goToActivity(AudioStreamingActivity::class.java)
+        }
+
+        binding.buttonVideo.setOnClickListener {
+            goToActivity(VideoStreamingActivity::class.java)
+        }
+
+        binding.buttonVideoLocal.setOnClickListener {
+            goToActivity(VideoPlayerLocalMainActivity::class.java)
+        }
+    }
+
+    private fun <T> goToActivity(destination: Class<T>) {
+        val intent = Intent(this, destination)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/coxtunes/coxtuneslivetv/VideoStreamingActivity.kt
@@ -0,0 +1,62 @@
+package com.coxtunes.coxtuneslivetv
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.coxtunes.coxtuneslivetv.databinding.ActivityVideoStreamingBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+
+class VideoStreamingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityVideoStreamingBinding
+    private var exoPlayer: ExoPlayer? = null
+    private var playbackPosition = 0L
+    private var playWhenReady = true
+    private val url = "https://tempx.jagobd.com:441/c5V6mmMyX7RpbEU9Mi8xNy8yMDEOGIDU6RgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcGVMZEJCTEFWeVN3PT0mdFsaWRtaW51aiPhnPTI/independent-8-org.stream/chunks.m3u8"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityVideoStreamingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        preparePlayer()
+    }
+
+    private fun preparePlayer() {
+        exoPlayer = ExoPlayer.Builder(this).build()
+        exoPlayer?.playWhenReady = true
+        binding.playerViewUrl.player = exoPlayer
+        val defaultHttpDataSourceFactory = DefaultHttpDataSource.Factory()
+        val mediaItem = MediaItem.fromUri(url)
+        val mediaSource = HlsMediaSource.Factory(defaultHttpDataSourceFactory).createMediaSource(mediaItem)
+        exoPlayer?.setMediaSource(mediaSource)
+        exoPlayer?.seekTo(playbackPosition)
+        exoPlayer?.playWhenReady = playWhenReady
+        exoPlayer?.prepare()
+    }
+
+    private fun releasePlayer() {
+        exoPlayer?.let { player ->
+            playbackPosition = player.currentPosition
+            playWhenReady = player.playWhenReady
+            player.release()
+            exoPlayer = null
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        releasePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releasePlayer()
+    }
+}
